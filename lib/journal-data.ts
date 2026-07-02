8fd618547faab3ef799c70402365fd0280e7e03e
RainingRainen <Rainensimpson@gmail.com>
2026-07-02 12:25:00 +0800

    Update journal-data.ts

--- lib/journal-data.ts
+++ lib/journal-data.ts
@@ -65,21 +65,5 @@ export const weeklyUpdates: WeeklyUpdate[] = [
     chart: "/charts/chart-3.png",
   },
 ]
-
-export type PortfolioLog = {
-  week: number
-  date: string
-  btcPrice: number
-  portfolioValue: number
-  change: number
-}
-
-export const portfolioLogs: PortfolioLog[] = [
-  { week: 20, date: "May 19", btcPrice: 62400, portfolioValue: 48200, change: 0 },
-  { week: 21, date: "May 26", btcPrice: 67800, portfolioValue: 52600, change: 9.13 },
-  { week: 22, date: "Jun 02", btcPrice: 71200, portfolioValue: 56900, change: 8.17 },
-  { week: 23, date: "Jun 09", btcPrice: 69100, portfolioValue: 55300, change: -2.81 },
-  { week: 24, date: "Jun 16", btcPrice: 66500, portfolioValue: 53100, change: -3.98 },
-  { week: 25, date: "Jun 23", btcPrice: 68900, portfolioValue: 55800, change: 5.08 },
-  { week: 26, date: "Jun 30", btcPrice: 73400, portfolioValue: 61500, change: 10.22 },
+eek: 26, date: "Jun 30", btcPrice: 73400, portfolioValue: 61500, change: 10.22 },
 ]
